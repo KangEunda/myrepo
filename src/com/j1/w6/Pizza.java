1cfbe030f9f70a30cd76609094cad8757e5a2273
unknown <>
2016-04-06 18:16:43 +0900

    w6 at 405

---
+++ src/com/j1/w6/Pizza.java
@@ -0,0 +1,33 @@
+package com.j1.w6;
+
+import java.util.ArrayList;
+
+abstract public class Pizza{
+  String name;
+  String dough;
+  String sauce;
+  ArrayList toppings = new ArrayList();
+  public void prepare(){
+    System.out.println("preparing" + name);
+  }
+  public void bake(){
+    System.out.println("baking" + name);
+  }
+  public void cut(){
+    System.out.println("cutting" + name);
+  }
+  public void box(){
+    System.out.println("boxing" + name);
+  }
+  public String toString() {
+  StringBuffer display = new StringBuffer();
+  display.append("---- " + name + " ----\n");
+  display.append(dough + "\n");
+  display.append(sauce + "\n");
+  for (int i = 0; i < toppings.size(); i++) {
+   display.append((String )toppings.get(i) + "\n");
+  }
+  return display.toString();
+ }
+
+}
