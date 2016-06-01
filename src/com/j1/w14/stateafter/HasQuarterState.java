92dc6945b92c2ff0287f2a5efe93661910f16436
unknown <>
2016-06-01 17:55:12 +0900

    w14 at 405

---
+++ src/com/j1/w14/stateafter/HasQuarterState.java
@@ -0,0 +1,28 @@
+package com.j1.w14.stateafter;
+
+import java.util.Random;
+
+public class HasQuarterState implements State {
+  GumballMachine gumballMachine;
+  
+  public HasQuarterState(GumballMachine gumballMachine){
+    this.gumballMachine = gumballMachine;
+  }
+  public void insertQuarter(){
+    System.out.println("You cannot insert another quarter.");
+  }
+  public void ejectQuarter(){
+    System.out.println("Quarter returned");
+    gumballMachine.setState(gumballMachine.getNoQuarterState());
+  }
+  public void turnCrank(){
+    System.out.println("You turned...");
+    gumballMachine.setState(gumballMachine.getSoldState());
+  }
+  public void dispense(){
+    System.out.println("No gumball dispensed");
+  }
+  public String toString(){
+    return "waiting for turn of crank";
+  }
+}
